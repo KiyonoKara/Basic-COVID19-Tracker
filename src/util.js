9b8e1d9e7e93a32ce4f7930032efeda66bd104a5
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-04-16 22:55:17 -0700

    Now with local GET requester

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
-// These are just other functions so I can display the data
-// It's optional but I recommend it
+const https = require('https');
+
 module.exports = {
     appendCommaToNumber: function(args) {
         return args.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -8,6 +8,17 @@ module.exports = {
     capitalizeFirstLetter: function(str) {
         return str.replace(/\w\S*/g, function(text) {
             return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+        });
+    },
+
+    GET: async function(url) {
+        return new Promise((resolve, reject) => {
+            https.get(url, callback => {
+                let buf = [];
+                callback.on('data', chunk => buf.push(chunk));
+                callback.on('error', error => reject(error));
+                callback.on('end', () => resolve(Buffer.concat(buf).toString()));
+            });
         });
     }
 }
